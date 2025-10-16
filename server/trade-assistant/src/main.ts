942359da79dbd4de53fb7fb7e45203d8d83dc9fb
atanasovv93 <nikolaatanasov93@yahoo.com>
2025-10-16 16:33:53 +0200

    main.ts server modified

--- server/trade-assistant/src/main.ts
+++ server/trade-assistant/src/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
@@ -7,8 +9,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Подели ја CORS варијаблата на листа
+  const allowedOrigins = (process.env.CORS_ORIGINS || '').split(',').map(origin => origin.trim());
+
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:4200',
+    origin: (origin, callback) => {
+      // Дозволи ако нема origin (nа постманино API calls) или ако е во allowedOrigins
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
@@ -24,6 +36,5 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`🚀 Server is running on http://0.0.0.0:${port}`);
-
 }
 bootstrap();
