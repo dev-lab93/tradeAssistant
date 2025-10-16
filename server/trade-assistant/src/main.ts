9f9837d7d2d86cabbe6642c47a0fb0ae24fa074d
atanasovv93 <nikolaatanasov93@yahoo.com>
2025-10-16 16:37:56 +0200

    last modify

--- server/trade-assistant/src/main.ts
+++ server/trade-assistant/src/main.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -9,12 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Подели ја CORS варијаблата на листа
-  const allowedOrigins = (process.env.CORS_ORIGINS || '').split(',').map(origin => origin.trim());
+  const allowedOrigins = (process.env.CORS_ORIGINS || '').split(',');
 
   app.enableCors({
     origin: (origin, callback) => {
-      // Дозволи ако нема origin (nа постманино API calls) или ако е во allowedOrigins
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
